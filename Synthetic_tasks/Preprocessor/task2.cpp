cb939692b66bb4085016f0e0af0da12c1134e317
Vladislav Znak <105151334+Znakous@users.noreply.github.com>
2025-03-25 01:11:09 +0300

    Update task2.cpp

--- Synthetic_tasks/Preprocessor/task2.cpp
+++ Synthetic_tasks/Preprocessor/task2.cpp
@@ -9,9 +9,9 @@ bool operator<(const std::string& lhs, const std::string& rhs) {
 
 
 int main() {
-    int a = 3, b = 4;
+    int a = 5, b = 4;
     std::string as = "aboba", bs = "abebra";
     int c = Max(a, b);
     std::string cs = Max(as, bs);
-    std::cout << c << " " << cs; // 4 abebra
+    std::cout << c << " " << cs; // 5 abebra
 }
